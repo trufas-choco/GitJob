b709db02c7f0d87f53b16260e84c13894ed9e6cd
fer <fcastrom@usm.cl>
2025-11-09 23:49:09 -0300

    creación de la clase producto y usuario (creo)

--- OficiosYA/app/models.py
+++ OficiosYA/app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class producto(models.Model):
+    usuario= models.ForeignKey(User, on_delete=models.CASCADE)
+    nombre= models.CharField(max_length=50)
+    precio= models.DecimalField(max_digits=10,decimal_places=2)
+    imagen= models.ImageField(upload_to='productos/', blank=True, null=True)
+    descripcion= models.TextField(blank=True)
+    fecha_publi= models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.nombre} - {self.usuario.username}"
+
+
+
+
